8182a593be29c7f5c0e7508676d16962134f5110
Praveen Ravichandran <me@pravinba9495.dev>
2020-07-18 14:31:49 -0400

    Updates linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,19 @@ module.exports = {
     es2020: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:prettier/recommended'
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 11,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'prettier',
-    'react',
-  ],
+  plugins: ["prettier", "react"],
   rules: {
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
   },
 };
